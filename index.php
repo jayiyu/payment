04186ca2b438ce9fbb1946b5448abf9bc2e6cb0f
jayi <2239317613@qq.com>
2019-11-01 14:19:04 +0800

    第一次提交

---
+++ index.php
@@ -0,0 +1,7 @@
+<?php 
+
+require_once "./vendor/autoload.php";
+
+use Xiaoy\Payment\Demo;
+
+echo Demo::test();
---
+++ src/Demo.php
@@ -0,0 +1,11 @@
+<?php 
+namespace Xiaoy\Payment;
+
+
+class Demo
+{
+	
+	public static function test(){
+		return "hello pay";
+	}
+}
